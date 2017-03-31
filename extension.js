f8979f2896022b107ed189308695b4782d607fa0
Andy C <andy.c@zoho.com>
2017-03-31 01:34:01 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,4 +1,27 @@
+/*
+ * IP Finder gnome extension
+ * https://github.com/LinxGem33/IP-Finder
+ * 
+ * Copyright (C) 2017 LinxGem33 (Andy C)
+ *
+ * This file is part of IP Finder gnome extension.
+ * IP Finder gnome extension is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * IP Finder gnome extension is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with IP Finder gnome extension.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
 /* -*- mode: js2; js2-basic-offset: 4; indent-tabs-mode: nil -*- */
+
 
 const Clutter = imports.gi.Clutter;
 const Gio = imports.gi.Gio;
