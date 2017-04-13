4c721dd0bb220391eda564b8370752435817567b
Jonatan Hatakeyama Zeidler <jonatan_zeidler@gmx.de>
2017-04-13 13:15:56 +0200

    Added Localization Support + German (de)

--- prefs.js
+++ prefs.js
@@ -31,7 +31,7 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const Convenience = Me.imports.convenience;
 
-const Gettext = imports.gettext.domain('gnome-shell-extensions');
+const Gettext = imports.gettext.domain('IP-Finder');
 const _ = Gettext.gettext;
 
 const SETTINGS_COMPACT_MODE = 'compact-mode';
@@ -79,7 +79,7 @@ let presentLabel = '<b>' + _("Display Options") + '</b>';
     positionContainer.pack_start(positionLabel, 0,0,0);
     positionContainer.pack_end(positionSelector, 0,0,0);
 
-    ['left','center','right'].forEach(function(item) {
+    [_("left"),_("center"),_("right")].forEach(function(item) {
       positionSelector.append_text(item);
     });
 
@@ -115,6 +115,7 @@ let presentLabel = '<b>' + _("Display Options") + '</b>';
 });
 
 function init() {
+  Convenience.initTranslations("IP-Finder");
 }
 
 function buildPrefsWidget() {
